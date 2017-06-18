97ae25d6db236cc0fa1c0937ed55f5f1cd14ecea
Charlie Gildawie <charlie.gildawie@skyscanner.net>
2017-06-18 07:52:54 +0100

    CLI for dembones using click

--- cli.py
+++ cli.py
@@ -1,16 +1,30 @@
 from dembones.collector import Collector
 import logging
+import click
 
 
-logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger("CLI")
+def initialise_logger(level):
+    logging.basicConfig(level=level)
+    return logging.getLogger("DEMBONES")
 
 
-def main():
-    log.info("Starting")
-    c = Collector(max_concurrent_fetches=5)
-    c.start_collection("https://blog.hartleybrody.com/")
+@click.group()
+def dembones():
+    pass
 
 
+@click.option("-mc,", "--max-concurrency", help="Max fetch tasks at any one time", default=5)
+@click.option("-md", "--max-depth", help="Maximum recursion when collecting URLS", default=3)
+@click.option("-v", "--verbose", help="Enable Verbose Mode", is_flag=True)
+@click.argument("url")
+def scrape(mc, md, v, url):
+    log = initialise_logger(logging.DEBUG if v else logging.INFO)
+
+    log.info("Starting Collection")
+    c = Collector(max_concurrent_fetches=mc, max_depth=md)
+    c.start_collection(url)
+
+
+# Simple helper for invoking in pycharm / ide for debugger
 if __name__ == "__main__":
-    main()
+    scrape(5, 3, True, "https://blog.hartleybrody.com/")
